b5d5e1abaa42faebd9d7cf86f9126916c01fc45c
tanthanadon <thanadon.bnk@gmail.com>
2020-03-31 13:44:37 +0700

    Unable to connect aws.mongo

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+
 import os
 import json
 from pathlib import Path
@@ -8,6 +10,16 @@ import pandas as pd
 from math import sin, cos, sqrt, atan2, radians
 
 app = Flask(__name__)
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://thanadon:tan@cluster0-ngydt.mongodb.net/test?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+    
+@app.route('/')
+def home_page():
+    online_users = mongo.db.users.find({"online": True})
+    return render_template("index.html",
+        online_users=online_users)
 
 @app.route('/addCard')
 def add_card():
